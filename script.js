ef6196b24b2f111f02ea404aae75c10864eb1411
sajal <sajalsbaral@gmail.com>
2023-11-24 13:39:18 -0500

    changed button values to the correct ones. Added checkWon function to see who reaached 5 points first. Cleaned up some logic.

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissor = document.querySelector('#scissor');
 const result = document.querySelector('#result');
-const retry = document.querySelector('#retry')
+const playAgain = document.querySelector('#playAgain');
+const container = document.querySelector('.container');
 let playerWon = 0;
 let computerWon = 0;
 
@@ -18,33 +19,58 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
+
     if (playerSelection === 'rock' && computerSelection === 'scissor' ||
     playerSelection === 'paper' && computerSelection === 'rock' || 
     playerSelection === 'scissor' && computerSelection === 'paper') {
         playerWon++;
-        return `You won! Player: ${playerWon} Computer: ${computerWon}`; 
+        return `The computer choose ${computerSelection}. You won! Player: ${playerWon} Computer: ${computerWon}`; 
     } else if (playerSelection === computerSelection) {
-        return `It's a draw! Player: ${playerWon} Computer: ${computerWon}`; 
+        return `The computer choose ${computerSelection}. It's a draw! Player: ${playerWon} Computer: ${computerWon}`; 
     } else {
         computerWon++;
-        return `You lost. . . Player: ${playerWon} Computer: ${computerWon}`;
+        return `The computer choose ${computerSelection}. You lost. . . Player: ${playerWon} Computer: ${computerWon}`;
     }  
 }
 
+function checkWon() {
+    if(playerWon == 5) {
+        rock.disabled = true;
+        paper.disabled = true;
+        scissor.disabled = true;
+        result.textContent = 'You reached 5 points first. Congrats!'
+    }
+    
+    if(computerWon == 5) {
+        rock.disabled = true;
+        paper.disabled = true;
+        scissor.disabled = true;
+        result.textContent = 'The computer reached 5 points first. Try again. . .!'
+    }
+}
+
 rock.addEventListener('click', () => {
-    return result.textContent = playRound(rock.value, getComputerChoice());
+    result.textContent = playRound(rock.value);
+    checkWon();
 });
 paper.addEventListener('click', () => {
-    return result.textContent = playRound(paper.value, getComputerChoice());
+    result.textContent = playRound(paper.value);
+    checkWon();
 });
 scissor.addEventListener('click', () => {
-    return result.textContent = playRound(scissor.value, getComputerChoice());
+    result.textContent = playRound(scissor.value);
+    checkWon();
 });
 
-retry.textContent = 'Retry';
-retry.addEventListener('click', () =>{
+playAgain.textContent = 'Play Again';
+playAgain.addEventListener('click', () =>{
     playerWon = 0;
     computerWon = 0;
+    rock.disabled = false;
+    paper.disabled = false;
+    scissor.disabled = false;
+
     return result.textContent = `Click to play. Player: ${playerWon} Computer: ${computerWon}`; 
 });
